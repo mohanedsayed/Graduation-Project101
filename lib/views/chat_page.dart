cb984695769258c80024ce7e94d4b93da3a7ae11
Mohaned Sayed <mohaned.sayed1@gmail.com>
2021-04-27 11:34:42 +0200

    working on chat screen

--- lib/views/chat_page.dart
+++ lib/views/chat_page.dart
@@ -1,10 +1,48 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Chat,'),
+    return Column(
+      children: [
+        CarouselSlider(
+            items: [
+              Column(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.green,
+                    maxRadius: 31,
+                    child: CircleAvatar(
+                      maxRadius: 21,
+                      backgroundColor: Colors.red,
+                    ),
+                  ),
+                  Text('Jeffery \n  Bizo')
+                ],
+              ),
+              CircleAvatar(
+                backgroundColor: Colors.yellow,
+              ),
+              CircleAvatar(
+                backgroundColor: Colors.green,
+              ),
+              CircleAvatar(
+                backgroundColor: Colors.black,
+              ),
+            ],
+            options: CarouselOptions(
+
+                // pauseAutoPlayOnManualNavigate: true,
+                // pauseAutoPlayInFiniteScroll: true,
+                // pauseAutoPlayOnTouch: true,
+                initialPage: 3,
+                pageSnapping: true,
+                enableInfiniteScroll: true,
+                height: 120,
+                aspectRatio: 0.4,
+                viewportFraction: 0.2)),
+      ],
     );
   }
 }
