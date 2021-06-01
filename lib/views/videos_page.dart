d32b80505fabc563984eb468c5df62f34fae409f
Mohaned Sayed <mohaned.sayed1@gmail.com>
2021-06-01 06:58:17 +0200

    foundation of videos

--- lib/views/videos_page.dart
+++ lib/views/videos_page.dart
@@ -4,7 +4,7 @@ class VideosPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
       drawer: Drawer(
         child: Icon(Icons.add_to_home_screen_outlined),
       ),
@@ -22,12 +22,41 @@ class VideosPage extends StatelessWidget {
             ],
           ),
           Container(
-              height: 200,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return VideoContainer();
-                  })),
+            height: 200,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return VideoContainer(
+                  height1: 140,
+                  width1: 200,
+                );
+              },
+            ),
+          ),
+          Divider(
+            height: 3,
+            thickness: 1,
+          ),
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.all(15),
+                child: Text('Videos'),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.only(left: 5),
+              scrollDirection: Axis.vertical,
+              itemBuilder: (context, index) {
+                return VideoContainer(
+                  height1: 200,
+                  width1: MediaQuery.of(context).size.width * 0.85,
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
@@ -36,6 +65,7 @@ class VideosPage extends StatelessWidget {
 
 class VideoContainer extends StatelessWidget {
   final double height1;
+
   final double width1;
   const VideoContainer({
     Key key,
@@ -52,16 +82,22 @@ class VideoContainer extends StatelessWidget {
           Stack(
             children: [
               Container(
-                height: 140,
-                width: 200,
+                height: height1,
+                width: width1,
                 decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 2,
+                    ),
+                  ],
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(18),
                 ),
               ),
               Container(
-                height: 70,
-                width: 200,
+                height: 0.5 * height1,
+                width: width1,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(18),
@@ -72,6 +108,8 @@ class VideoContainer extends StatelessWidget {
                 top: 45,
                 right: 3,
                 child: Card(
+                  // borderOnForeground: false,
+                  // elevation: 2,
                   margin: EdgeInsets.all(5),
                   color: Colors.grey[700],
                   child: Text(
