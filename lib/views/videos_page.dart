c73fe6b0f7e6dd8c02ddbbfc96772c23784582ca
Mohaned Sayed <mohaned.sayed1@gmail.com>
2021-06-06 06:44:32 +0200

    m

--- lib/views/videos_page.dart
+++ lib/views/videos_page.dart
@@ -5,11 +5,15 @@ class VideosPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      drawer: Drawer(
-        child: Icon(Icons.add_to_home_screen_outlined),
-      ),
+      // drawer: Drawer(
+      //   child: Icon(Icons.add_to_home_screen_outlined),
+      // ),
       appBar: AppBar(
-        title: Text('Videos'),
+        backgroundColor: Colors.white,
+        title: Text(
+          'Videos',
+          style: TextStyle(color: Colors.black, fontSize: 24),
+        ),
       ),
       body: Column(
         children: [
@@ -50,9 +54,11 @@ class VideosPage extends StatelessWidget {
               padding: EdgeInsets.only(left: 5),
               scrollDirection: Axis.vertical,
               itemBuilder: (context, index) {
-                return VideoContainer(
-                  height1: 200,
-                  width1: MediaQuery.of(context).size.width * 0.85,
+                return Center(
+                  child: VideoThumbnail(
+                    height1: 200,
+                    width1: MediaQuery.of(context).size.width * 0.85,
+                  ),
                 );
               },
             ),
@@ -114,7 +120,7 @@ class VideoContainer extends StatelessWidget {
                   color: Colors.grey[700],
                   child: Text(
                     '20:30',
-                    style: TextStyle(color: Colors.white, fontSize: 9),
+                    style: TextStyle(color: Colors.white, fontSize: 12),
                   ),
                 ),
               ),
@@ -123,7 +129,7 @@ class VideoContainer extends StatelessWidget {
                 left: 8,
                 child: Text(
                   'Iron Man Suit up scene',
-                  style: TextStyle(fontSize: 15),
+                  style: TextStyle(fontSize: 16),
                 ),
               ),
               Positioned(
@@ -132,7 +138,88 @@ class VideoContainer extends StatelessWidget {
                 child: Text(
                   'John Jackson - Today, 04:30',
                   textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 9),
+                  style: TextStyle(fontSize: 12),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class VideoThumbnail extends StatelessWidget {
+  final double height1;
+
+  final double width1;
+  const VideoThumbnail({
+    Key key,
+    this.height1,
+    this.width1,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+      child: Row(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: height1,
+                width: width1,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 3,
+                      spreadRadius: .5,
+                    ),
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(18),
+                ),
+              ),
+              Container(
+                height: 0.5 * height1,
+                width: width1,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(18),
+                        topRight: Radius.circular(18)),
+                    color: Colors.red),
+              ),
+              Positioned(
+                top: 65,
+                right: 10,
+                child: Card(
+                  // borderOnForeground: false,
+                  // elevation: 2,
+                  margin: EdgeInsets.all(5),
+                  color: Colors.grey[700],
+                  child: Text(
+                    '20:30',
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 50,
+                left: 8,
+                child: Text(
+                  'Iron Man Suit up scene',
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+              Positioned(
+                bottom: 25,
+                left: 8,
+                child: Text(
+                  'John Jackson - Today, 04:30',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16),
                 ),
               ),
             ],
