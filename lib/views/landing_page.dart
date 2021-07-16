982c0b784d809e2de9a74a7617cb73322e2bc7f2
Mohaned Sayed <mohaned.sayed1@gmail.com>
2021-07-16 15:16:54 +0200

    Revert "Revert "Revert "added firebase auth"""
    This reverts commit 63a3c6fee0f8d64b98d88444176b5be08a45c318.

--- lib/views/landing_page.dart
+++ lib/views/landing_page.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:graduation_project101/constants/colors.dart';
